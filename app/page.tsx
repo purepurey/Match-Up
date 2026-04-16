572348f39c68dba3db5546bcb2b440254a041a16
purepurey <chilledactive@gmail.com>
2026-04-16 15:06:12 +0700

    Version #2.2
    - Bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage() {
       </div>
 
       {/* Sign In Button */}
-      <Link href="/home" className="w-full bg-[#162D3A] text-white py-4 font-semibold text-base mb-6 block text-center">
+      <Link href="/home" className="w-full bg-[#162D3A] text-white py-4 font-light text-base mb-6 block text-center">
       Sign in
       </Link>
 
@@ -54,8 +54,8 @@ export default function LoginPage() {
       {/* Social Buttons */}
 
       {/* Google */}
-      <div className="flex gap-3 mb-6">
-        <button className="flex-1 flex items-center justify-center gap-2 bg-[#F3F2EB] py-3 text-sm font-medium text-[#294957]">
+      <div className="flex gap-3 mb-6 ">
+        <button className="flex-1 flex items-center justify-center gap-2 bg-[#F3F2EB] py-3 text-sm font-medium text-[#294957] shadow-[0px_4px_0px_0px_rgba(0,0,0,0.25)]">
           <svg width="20" height="20" viewBox="0 0 24 24">
             <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
             <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
@@ -66,7 +66,7 @@ export default function LoginPage() {
         </button>
 
         {/* Facebook */}
-        <button className="flex-1 flex items-center justify-center gap-2 bg-[#F3F2EB] py-3 text-sm font-medium text-[#294957]">
+        <button className="flex-1 flex items-center justify-center gap-2 bg-[#F3F2EB] py-3 text-sm font-medium text-[#294957] shadow-[0px_4px_0px_0px_rgba(0,0,0,0.25)]">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="#1877F2">
             <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
           </svg>
